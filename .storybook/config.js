0ec99041ec5e5eadb0a196879c25a150a247cf32
DANIEL <yuntaehui77@gmail.com>
2022-01-30 00:44:32 +0900

    스토리북 스토리 자동추가 및 애드온 addon-actions, addon-jsx 추가

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,13 @@
-import {configure} from '@storybook/react'
+import {configure, setAddon} from '@storybook/react'
+import interopRequireDefault from 'babel-runtime/helpers/interopRequireDefault'
+import JSXAddon from 'storybook-addon-jsx'
 
 function loadStories() {
-  // 스토리 파일을 이곳에 추가할 수 있습니다.
-  require('../src/stories/InputStory')
-  require('../src/stories/NewCounterStory')
+  const context = require.context('../src/stories', true, /Story\.jsx$/);
+  context.keys().forEach((srcFile) => {
+    interopRequireDefault(context(srcFile));
+  })
 }
 
+setAddon(JSXAddon)
 configure(loadStories, module)
--- src/stories/InputStory.jsx
+++ src/stories/InputStory.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
+import {action} from '@storybook/addon-actions'
 
 import Input from '../03/Input'
 
 storiesOf('Input', module)
-  .add('기본 설정', () => <Input name="name"/>)
-  .add('label 예제', () => <Input name="name" label="이름"/>)
+  .addWithJSX('기본 설정', () => <Input name="name"/>)
+  .addWithJSX('label 예제', () => <Input name="name" label="이름"/>)
+  .addWithJSX('onChange 예제', () => <Input name="name" onChange={action('onChange 이벤트 발생')}/>)
