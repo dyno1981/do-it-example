daa8ebb55da47f82165093937500525632c947d7
DANIEL <yuntaehui77@gmail.com>
2022-01-30 00:00:21 +0900

    README 수정 및 스토리 추가

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
 import {configure} from '@storybook/react'
 
 function loadStories() {
-  require('../src/stories/InputStory')
   // 스토리 파일을 이곳에 추가할 수 있습니다.
+  require('../src/stories/InputStory')
+  require('../src/stories/NewCounterStory')
 }
 
 configure(loadStories, module)
--- src/stories/InputStory.jsx
+++ src/stories/InputStory.jsx
@@ -3,4 +3,6 @@ import {storiesOf} from '@storybook/react'
 
 import Input from '../03/Input'
 
-storiesOf('Input', module).add('기본 설정', () => <Input name="name"/>)
+storiesOf('Input', module)
+  .add('기본 설정', () => <Input name="name"/>)
+  .add('label 예제', () => <Input name="name" label="이름"/>)
